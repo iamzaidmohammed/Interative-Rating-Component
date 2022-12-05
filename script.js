762639c2547aa8b222d0fb48310acb6d1ff151a2
Zaid Mohammed Zainudeen <zainudeenzaidmohammed7@gmail.com>
2022-12-05 13:08:45 +0000

    Updated whole project

--- script.js
+++ script.js
@@ -1,60 +1,133 @@
-const firstRate = document.querySelector('.rating');
-const secondRate = document.querySelector('.thanks');
-const selected = document.querySelector('.output');
-const submitButton = document.querySelector('.submit');
-//const options1 = document.getElementsByName('choice');
-let one = document.getElementById('one');
-let two = document.getElementById('two');
-let three = document.getElementById('three');
-let four = document.getElementById('four');
-let five = document.getElementById('five');
+const rate1 = document.getElementById("rate1");
+const rate2 = document.getElementById("rate2");
+const rate3 = document.getElementById("rate3");
+const rate4 = document.getElementById("rate4");
+const rate5 = document.getElementById("rate5");
 
+rate1.addEventListener("click", (e) => {
+  if (
+    rate2.classList.contains("clicked") ||
+    rate3.classList.contains("clicked") ||
+    rate4.classList.contains("clicked") ||
+    rate5.classList.contains("clicked")
+  ) {
+    rate3.classList.remove("clicked");
+    rate2.classList.remove("clicked");
+    rate4.classList.remove("clicked");
+    rate5.classList.remove("clicked");
+    rate1.classList.add("clicked");
+  } else {
+    rate1.classList.toggle("clicked");
+  }
+});
 
-function showOption(){
-   /* for( i = 0; i < options1.length; i++ ){
-        if(options1[i]){
-        selected.innerText = `You selected ${options1[i].value} out of 5`
-        
+rate2.addEventListener("click", (e) => {
+  if (
+    rate1.classList.contains("clicked") ||
+    rate3.classList.contains("clicked") ||
+    rate4.classList.contains("clicked") ||
+    rate5.classList.contains("clicked")
+  ) {
+    rate1.classList.remove("clicked");
+    rate3.classList.remove("clicked");
+    rate4.classList.remove("clicked");
+    rate5.classList.remove("clicked");
+    rate2.classList.add("clicked");
+  } else {
+    rate2.classList.toggle("clicked");
+  }
+});
 
+rate3.addEventListener("click", (e) => {
+  if (
+    rate1.classList.contains("clicked") ||
+    rate2.classList.contains("clicked") ||
+    rate4.classList.contains("clicked") ||
+    rate5.classList.contains("clicked")
+  ) {
+    rate1.classList.remove("clicked");
+    rate2.classList.remove("clicked");
+    rate4.classList.remove("clicked");
+    rate5.classList.remove("clicked");
+    rate3.classList.add("clicked");
+  } else {
+    rate3.classList.toggle("clicked");
+  }
+});
+
+rate4.addEventListener("click", (e) => {
+  if (
+    rate1.classList.contains("clicked") ||
+    rate2.classList.contains("clicked") ||
+    rate3.classList.contains("clicked") ||
+    rate5.classList.contains("clicked")
+  ) {
+    rate1.classList.remove("clicked");
+    rate2.classList.remove("clicked");
+    rate3.classList.remove("clicked");
+    rate5.classList.remove("clicked");
+    rate4.classList.add("clicked");
+  } else {
+    rate4.classList.toggle("clicked");
+  }
+});
+
+rate5.addEventListener("click", (e) => {
+  if (
+    rate2.classList.contains("clicked") ||
+    rate3.classList.contains("clicked") ||
+    rate4.classList.contains("clicked") ||
+    rate1.classList.contains("clicked")
+  ) {
+    rate1.classList.remove("clicked");
+    rate2.classList.remove("clicked");
+    rate3.classList.remove("clicked");
+    rate4.classList.remove("clicked");
+    rate5.classList.add("clicked");
+  } else {
+    rate5.classList.toggle("clicked");
+  }
+});
+
+const rates = document.querySelectorAll(".rate");
+const rateCard = document.getElementById("rate__card");
+const selected = document.getElementById("selected");
+const thankYouCard = document.getElementById("thank__you__card");
+const submitBtn = document.getElementById("btn");
+
+submitBtn.addEventListener("click", (e) => {
+  if (
+    rate1.classList.contains("clicked") ||
+    rate2.classList.contains("clicked") ||
+    rate3.classList.contains("clicked") ||
+    rate4.classList.contains("clicked") ||
+    rate5.classList.contains("clicked")
+  ) {
+    for (let rate = 0; rate < rates.length; rate++) {
+      if (rates[rate].classList.contains("clicked")) {
+        switch (rate) {
+          case 0:
+            selected.innerText = 1;
+            break;
+          case 1:
+            selected.innerText = 2;
+            break;
+          case 2:
+            selected.innerText = 3;
+            break;
+          case 3:
+            selected.innerText = 4;
+            break;
+          case 4:
+            selected.innerText = 5;
+            break;
+
+          default:
+            break;
         }
-        else{
-            alert('select a number');
-        }
-    
+      }
     }
-    */
-   if(one){
-    selected.innerHTML = `You selected ${one.value} out of 5`;
-   }
-   else if(two){
-    selected.innerText = `You selected ${two.value} out of 5`;
-   }
-   else if(three){
-    selected.innerHTML = `You selected ${three.value} out of 5`;
-   }
-   else if(four){
-    selected.innerHTML = `You selected ${four.value} out of 5`;
-   }
-   else if(five){
-    selected.innerHTML = `You selected ${five.value} out of 5`;
-   } 
-   else{
-    alert('leave')
-   }
-
-
-}
-
-console.log(showOption());
-
-
-    submitButton.addEventListener('click', function(e){
-        e.preventDefault();
-        firstRate.style.display = 'none';
-        secondRate.style.display = 'block';
-
-        showOption();
-    });
-
-
-
+    rateCard.style.display = "none";
+    thankYouCard.style.display = "block";
+  }
+});
